09b201d746a66b6a1ed5c4df21cb955e2bf0e79f
Angela Weber <angelaweber95@hotmail.com>
2020-01-10 15:03:18 -0800

    pass test for find method in volunteer class. Add test for find method in project class

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -32,4 +32,16 @@ class Volunteer
     result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', '#{@project_id}') RETURNING id;")
     @id = result.first().fetch("id").to_i
   end
+
+  def self.find(id)
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
+    if volunteer
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      Volunteer.new({:name => name, :project_id => project_id, :id => id})
+    else
+      nil
+    end
+  end
 end
