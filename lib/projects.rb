e6405a9825ae83e0d6f38678ede2b4193978e870
Angela Weber <angelaweber95@hotmail.com>
2020-01-10 10:02:24 -0800

    create all needed files, with test to see if project.new works

---
+++ lib/projects.rb
@@ -0,0 +1,11 @@
+class Project
+  attr_accessor :title, :id
+
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
+  end
+
+  
+
+end
---
+++ lib/volunteers.rb
@@ -0,0 +1,10 @@
+class Volunteers
+  attr_accessor :name, :id, :project_id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+    @project_id = attributes.fetch(:project_id)
+  end
+
+end
