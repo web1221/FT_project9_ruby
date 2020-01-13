9d19c5dff68f789914a8aa047029629d18d528c0
Angela Weber <angelaweber95@hotmail.com>
2020-01-12 16:08:55 -0800

    all test are passing

--- app.rb
+++ app.rb
@@ -1,78 +1,3 @@
-# require('sinatra')
-# require('sinatra/reloader')
-# require('./lib/project')
-# require('./lib/volunteer')
-# require('pry')
-# also_reload('lib/**/*.rb')
-# require('pg')
-#
-# DB = PG.connect({:dbname => 'volunteer_tracker'})
-#
-# get('/') do
-#   @projects = Project.all
-#   erb(:projects)
-# end
-#
-# get('/projects') do
-#   @projects = Project.all
-#   erb(:projects)
-# end
-#
-# post('/projects/new') do
-#   title = params[:title_input]
-#   @project = Project.new({:title => title, :id => nil})
-#   @project.save()
-#   @projects = Project.all
-#   erb(:projects)
-# end
-#
-# get('/projects/:id') do
-#   @project = Project.find(params[:id].to_i())
-#   if @project == nil
-#     erb(:go_back)
-#   else
-#     erb(:project)
-#   end
-# end
-#
-# post('/projects/:id/volunteers') do
-#   @project = Project.find(params[:id].to_i())
-#   volunteer = Volunteer.new({:name => params[:name_input], :project_id => @project.id, :id => nil})
-#   volunteer.save()
-#   erb(:project)
-# end
-#
-# get('/projects/:id/edit') do
-#   @project = Project.find(params[:id].to_i())
-#   erb(:edit_project)
-# end
-#
-# patch('/projects/:id') do
-#   @project = Project.find(params[:id].to_i())
-#   @project.update(params[:title_input])
-#   @projects = Project.all
-#   erb(:project)
-# end
-#
-# delete('/projects/:id') do
-#   @project = Project.find(params[:id].to_i())
-#   @project.delete()
-#   redirect to('/projects')
-# end
-#
-#
-# get('/projects/:id/volunteers/:volunteer_id') do
-#   @volunteer = Volunteer.find(params[:volunteer_id].to_i())
-#   erb(:volunteer)
-# end
-#
-# patch('/projects/:id/volunteers/:volunteer_id') do
-#   @project = Project.find(params[:id].to_i())
-#   volunteer = Volunteer.find(params[:volunteer_id].to_i())
-#   volunteer.update(params[:name_input], @project.id)
-#   erb(:project)
-# end
-
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/project')
@@ -94,14 +19,11 @@ get('/projects') do
 end
 
 post('/projects') do
-  project = Project.new({:title => params[:title_input], :id => nil})
-  project.save()
-  redirect to('/projects')
-end
-
-get('/projects/new') do
+  title = params[:title_input]
+  @project = Project.new({:title => title, :id => nil})
+  @project.save()
   @projects = Project.all
-  erb(:new_project)
+  erb(:projects)
 end
 
 get('/projects/:id') do
@@ -138,6 +60,7 @@ delete('/projects/:id') do
   redirect to('/projects')
 end
 
+
 get('/projects/:id/volunteers/:volunteer_id') do
   @volunteer = Volunteer.find(params[:volunteer_id].to_i())
   erb(:volunteer)
@@ -147,6 +70,83 @@ patch('/projects/:id/volunteers/:volunteer_id') do
   @project = Project.find(params[:id].to_i())
   volunteer = Volunteer.find(params[:volunteer_id].to_i())
   volunteer.update(params[:name_input], @project.id)
-  binding.pry
   erb(:project)
 end
+
+# require('sinatra')
+# require('sinatra/reloader')
+# require('./lib/project')
+# require('./lib/volunteer')
+# require('pry')
+# also_reload('lib/**/*.rb')
+# require('pg')
+#
+# DB = PG.connect({:dbname => 'volunteer_tracker'})
+#
+# get('/') do
+#   @projects = Project.all
+#   erb(:projects)
+# end
+#
+# get('/projects') do
+#   @projects = Project.all
+#   erb(:projects)
+# end
+#
+# post('/projects') do
+#   project = Project.new({:title => params[:title_input], :id => nil})
+#   project.save()
+#   redirect to('/projects')
+# end
+#
+# get('/projects/new') do
+#   @projects = Project.all
+#   erb(:new_project)
+# end
+#
+# get('/projects/:id') do
+#   @project = Project.find(params[:id].to_i())
+#   if @project == nil
+#     erb(:go_back)
+#   else
+#     erb(:project)
+#   end
+# end
+#
+# post('/projects/:id/volunteers') do
+#   @project = Project.find(params[:id].to_i())
+#   volunteer = Volunteer.new({:name => params[:name_input], :project_id => @project.id, :id => nil})
+#   volunteer.save()
+#   erb(:project)
+# end
+#
+# get('/projects/:id/edit') do
+#   @project = Project.find(params[:id].to_i())
+#   erb(:edit_project)
+# end
+#
+# patch('/projects/:id') do
+#   @project = Project.find(params[:id].to_i())
+#   @project.update(params[:title_input])
+#   @projects = Project.all
+#   erb(:project)
+# end
+#
+# delete('/projects/:id') do
+#   @project = Project.find(params[:id].to_i())
+#   @project.delete()
+#   redirect to('/projects')
+# end
+#
+# get('/projects/:id/volunteers/:volunteer_id') do
+#   @volunteer = Volunteer.find(params[:volunteer_id].to_i())
+#   erb(:volunteer)
+# end
+#
+# patch('/projects/:id/volunteers/:volunteer_id') do
+#   @project = Project.find(params[:id].to_i())
+#   volunteer = Volunteer.find(params[:volunteer_id].to_i())
+#   volunteer.update(params[:name_input], @project.id)
+#   binding.pry
+#   erb(:project)
+# end
